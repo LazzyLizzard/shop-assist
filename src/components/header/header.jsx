c2dc01937b816f8407ccf7df73d77b29f6b767f9
Lazy Lizard <lazy.lizard@yandex.ru>
2019-03-30 23:22:30 +0300

    Components reorder

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import './header.scss';
 
 export const Header = () => (
-    <div className="header">Shop assist</div>
+    <Grid container spacing={24}>
+        <Grid item xs={12}>
+            <div className="header">Shop assist</div>
+        </Grid>
+    </Grid>
 );
