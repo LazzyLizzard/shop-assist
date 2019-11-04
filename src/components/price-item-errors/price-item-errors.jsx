49c7a54b8cbcf6892310b096c7c4735e6a6d4889
Lazy Lizard <lazy.lizard@yandex.ru>
2019-11-04 20:56:15 +0300

    Hooks refactoring
    * primary errors messages it price items styled

--- src/components/price-item-errors/price-item-errors.jsx
+++ src/components/price-item-errors/price-item-errors.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, makeStyles} from '@material-ui/core';
+import {Grid, makeStyles, Typography} from '@material-ui/core';
 import {red} from '@material-ui/core/colors';
 
 const colors = {
@@ -18,7 +18,11 @@ export const PriceItemErrors = ({errors = []}) => {
     return errors.length
         ? (
             <Grid container className={classes.root}>
-                <Grid item>Поля с ошибками должны содержать только символы от 1 до 9 и ".", например 125 или 10.4</Grid>
+                <Grid item>
+                    <Typography variant="body2" gutterBottom>
+                        Поля с ошибками должны содержать только символы от 1 до 9 и ".", например 125 или 10.4
+                    </Typography>
+                </Grid>
             </Grid>
         )
         : null
